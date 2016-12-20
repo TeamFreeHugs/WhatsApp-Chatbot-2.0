d21b569c2233e0f981bd8f1eb7c80644275615c3
Eyeball Edward <edwardyeung.private@gmail.com>
2016-12-20 17:25:41 +0800

    Can now turn letters to numbers

--- spec/checksumutils_spec.js
+++ spec/checksumutils_spec.js
@@ -1,22 +1,49 @@
+function padZeros(array) {
+    return [0].concat(array).slice(-2);
+}
+
 function prefix(prefix) {
-    return [0].concat([...prefix]).slice(-2).join('');
+    return padZeros([...prefix]).join("");
+}
+
+function numPrefix(prefix) {
+    var p;
+    if (prefix.startsWith("0")) {
+        p = prefix.substring(1);
+    } else {
+        p = prefix;
+    }
+    return padZeros([...p].map(c => c.codePointAt(0) - 64));
 }
 
 describe("Singapore Car Plate Checksum", function() {
     describe("Prefix", function() {
-          it("selects last 2 of 3 letters", function() {
-              var my_prefix = "SJG";
-              expect(prefix(my_prefix)).toBe("JG");
-          });
+        describe("Letter selection", function() {
+            it("selects last 2 of 3 letters", function() {
+                var myPrefix = "SJG";
+                expect(prefix(myPrefix)).toEqual("JG");
+            });
 
-          it("returns both letters for 2-letter prefixes", function() {
-              var my_prefix = "SE";
-              expect(prefix(my_prefix)).toBe("SE");
-          });
+            it("returns both letters for 2-letter prefixes", function() {
+                var myPrefix = "SE";
+                expect(prefix(myPrefix)).toEqual("SE");
+            });
 
-          it("returns zero-padded letter", function() {
-              var my_prefix = "E";
-              expect(prefix(my_prefix)).toBe("0E");
-          });
+            it("returns zero-padded letter", function() {
+                var myPrefix = "E";
+                expect(prefix(myPrefix)).toEqual("0E");
+            });
+        });
+
+        describe("Convert to numbers", function() {
+            it("maps letters to numbers", function() {
+                var myPrefix = "JG";
+                expect(numPrefix(myPrefix)).toEqual([10, 7]);
+            });
+            it("returns leading 0 and maps remaining letters to numbers", function() {
+                var myPrefix = "0E";
+                expect(numPrefix(myPrefix)).toEqual([0, 5]);
+            });
+        });
     });
 });
