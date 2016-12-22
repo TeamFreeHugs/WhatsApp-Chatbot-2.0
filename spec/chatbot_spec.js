dfd7bdc6e1f72493e230f5bb3e15d962e79dd993
Eyeball Edward <edwardyeung.private@gmail.com>
2016-12-22 16:21:46 +0800

    Testing

--- spec/chatbot_spec.js
+++ spec/chatbot_spec.js
@@ -1,29 +1,47 @@
 // This uses a custom message send/receive system
 
 const WhatsAppAPI = require('../lib/WhatsAppAPI');
+const WhatsAppMessageReciever = require('../lib/reciever/WhatsAppMessageReciever');
+const WhatsAppMessageSender = require('../lib/sender/WhatsAppMessageSender');
+const NewMessageEvent = require('../lib/events/NewMessageEvent');
+const Message = require('../lib/Message');
 
-class FakeSender {
+class FakeSender extends WhatsAppMessageSender {
+    constructor() {
+        super();
+    }
+
     sendMessage(message) {
-        reallySent(message);
+        this.reallySent = true;
     }
 }
 
-class FakeReceiver {
+class FakeReceiver extends WhatsAppMessageReciever {
+
+    constructor() {
+        super();
+    }
+
     get isReady() {
         return true;
     }
 
-    startWatching() {
-        
+    startGettingMessages() {
+        this.emit('message', new NewMessageEvent(new Message('Hello world!', 'Eyeball', null, null)));
     }
+
 }
 
 function createAPI() {
-    return new WhatsAppAPI(new FakeSender, new FakeReceiver);
+    return new WhatsAppAPI(new FakeSender(), new FakeReceiver());
 }
 
 describe('WhatsApp API', () => {
     it('can receive messages', () => {
         const api = createAPI();
+        var event;
+        api.on('message', e => event = e);
+        api.startGettingMessages();
+        expect(event.message.content).toEqual('Hello world!');
     });
 });
